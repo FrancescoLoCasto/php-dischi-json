1169e9fee8dab52262c91a21f95b03da81002314
MyUsername <my@email.com>
2023-01-27 18:44:36 +0100

    if implement

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-
-const {createApp} = Vue;
+const { createApp } = Vue;
 createApp({
-	data() {
-		return{
-			
-		}
-	},
-}).mount ('#app');
+    data() {
+        return {
+            apiURL: "./server.php",
+            diskList: [],
+        }
+    },
+    methods: {
+        getAPI() {
+            axios.get(this.apiURL)
+              .then((response) => {
+                console.log(response.data);
+                this.diskList = response.data;
+                 console.log(this.diskList);
+            });
+        },
+    },
+    created() {
+        this.getAPI();
+    },
+}).mount('#app');
