5e09ef876840c4a118960c4f581b130b64aca232
MyUsername <my@email.com>
2023-01-30 16:48:57 +0100

    Fix server php code

--- server.php
+++ server.php
@@ -3,7 +3,8 @@
 $getJson = file_get_contents(__DIR__ . '/db/dischi.json');
 $array = json_decode($getJson, true);
 
-if ($array != null) {
-   header('Content-Type: application/json');
-   echo json_encode($array);
+if ($array == null) {
+  $array = [];
 }
+header('Content-Type: application/json');
+echo json_encode($array);
